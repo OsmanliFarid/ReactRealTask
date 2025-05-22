28254f669aea70a2d160ae00867f6a4830b1ec44
OsmanliFarid <osmanliferid686@gmail.com>
2025-05-23 01:40:38 +0400

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div className="bg-red-600 max-w-[85vw] mx-[auto]">
+        <div className="flex justify-center ">
+          <div className="bg-green-500 w-[300px]">1</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
